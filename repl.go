965375f85d72246898c2706bf04b8eafae741a66
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-20 08:50:05 +0700

    Added first commands, help and exit

--- repl.go
+++ repl.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+type cliCommand struct {
+    name string
+    description string
+    callback func() error
+}
+
+func getCliCommands() map[string]cliCommand {
+    return map[string]cliCommand{
+        "help": {
+            name: "help",
+            description: "Displays a help message",
+            callback: commandHelp,
+        },
+        "exit": {
+            name: "exit",
+            description: "Exit the Pokedex",
+            callback: commandExit,
+        },
+    }
+}
+
+func commandHelp() error {
+    fmt.Println("here is some help")
+    return nil
+}
+
+func commandExit() error {
+    fmt.Println("Exiting...")
+    os.Exit(0)
+    return nil
+}
 
 func startRepl() {
-    fmt.Println("Welcome to the pokedex!")
+    scanner := bufio.NewScanner(os.Stdin)
+    cliCommands := getCliCommands()
+
+    for {
+
+        fmt.Println("")
+        fmt.Println("Welcome to the pokedex!")
+        fmt.Println("Usage:")
+        fmt.Println("")
+
+        for command := range cliCommands {
+            fmt.Println(cliCommands[command].name)
+        }
+
+        fmt.Println("")
+        fmt.Print("Pokedex > ")
+
+        scanner.Scan()
+        input := scanner.Text()
+
+        err := cliCommands[input].callback()
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+    }
 }
