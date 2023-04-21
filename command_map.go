69c32546253920c650bf09af495d14926c5f84db
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-21 10:18:59 +0700

    Add pokeapi package

--- command_map.go
+++ command_map.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/emilmalmsten/pokedex_cli/internal/pokeapi"
+    "log"
+)
 
 func commandMap() error {
 
-    fmt.Println("here are some areas")
+    locationAreasResp, err := pokeapi.ListLocationAreas()
+    if err != nil {
+        log.Fatal(err)
+    }
+    for _, area := range locationAreasResp.Results {
+        fmt.Println(area.Name)
+    }
+    return nil
+
+}
+
+func commandMapb() error {
+
+    fmt.Println("Last 20 areas")
     return nil
 
 }
---
+++ internal/pokeapi/pokeapi.go
@@ -0,0 +1,47 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const (
+    baseURL = "https://pokeapi.co/api/v2"
+)
+
+type LocationAreasResp struct {
+	Count    int    `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func ListLocationAreas() (LocationAreasResp, error) {
+    locationAreasResp := LocationAreasResp{}
+    endpoint := "/location-area"
+    fullURL := baseURL + endpoint
+    res, err := http.Get(fullURL)
+    if err != nil {
+        return locationAreasResp, err
+    }
+    body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+        return locationAreasResp, fmt.Errorf("Response failed with status code: %v", res.StatusCode)
+	}
+	if err != nil {
+        return locationAreasResp, err
+	}
+
+    err = json.Unmarshal(body, &locationAreasResp)
+    if err != nil {
+        return locationAreasResp, err
+    }
+
+    return locationAreasResp, nil
+}
