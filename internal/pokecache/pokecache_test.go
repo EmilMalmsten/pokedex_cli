a6203f071ce388c9cb9f06018ff4f57405d2e68a
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-22 11:15:44 +0700

    add cache reap tests

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -2,17 +2,18 @@ package pokecache
 
 import (
 	"testing"
+	"time"
 )
 
 func TestCreateCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Minute * 15)
 	if cache.cache == nil {
 		t.Error("cache is nil")
 	}
 }
 
 func TestAddGet(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Minute * 15)
 
 	cases := []struct {
 		inputKey string
@@ -51,4 +52,36 @@ func TestAddGet(t *testing.T) {
 		}
 
 	}
+}
+
+func TestReapCache(t *testing.T) {
+	cache := NewCache(time.Second * 3)
+	cache.Add("key1", []byte("val"))
+
+	if _, ok := cache.cache["key1"]; !ok {
+		t.Error("added entry does not exist")
+	}
+
+	time.Sleep(5 * time.Second)
+
+	if _, ok := cache.cache["key1"]; ok {
+		t.Error("added entry did not get reaped")
+	}
+
+}
+
+func TestReapCacheFail(t *testing.T) {
+	cache := NewCache(time.Second * 3)
+	cache.Add("key1", []byte("val"))
+
+	if _, ok := cache.cache["key1"]; !ok {
+		t.Error("added entry does not exist")
+	}
+
+	time.Sleep(2 * time.Second)
+
+	if _, ok := cache.cache["key1"]; !ok {
+		t.Error("added entry should not have been reaped")
+	}
+
 }
