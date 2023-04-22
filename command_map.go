d64b74bc65259c62b81c1655b55dc2b32691c083
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-22 17:56:52 +0700

    add possibility to add args with command

--- command_map.go
+++ command_map.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-func commandMap(cfg *config) error {
+func commandMap(cfg *config, args ...string) error {
 	locationAreasResp, err := cfg.pokeapiClient.ListLocationAreas(cfg.nextLocationAreaURL)
 	if err != nil {
 		log.Fatal(err)
@@ -19,7 +19,7 @@ func commandMap(cfg *config) error {
 
 }
 
-func commandMapb(cfg *config) error {
+func commandMapb(cfg *config, args ...string) error {
 	locationAreasResp, err := cfg.pokeapiClient.ListLocationAreas(cfg.prevLocationAreaURL)
 
 	if err != nil {
--- repl.go
+++ repl.go
@@ -4,12 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, ...string) error
 }
 
 func getCliCommands() map[string]cliCommand {
@@ -34,21 +37,26 @@ func getCliCommands() map[string]cliCommand {
 			description: "Display the last 20 areas",
 			callback:    commandMapb,
 		},
+		"explore": {
+			name:        "explore",
+			description: "Explore the pokemon within an area",
+			callback:    commandExplore,
+		},
 	}
 }
 
-/* func clearTerminal() {
-    var cmd *exec.Cmd
+func clearTerminal() {
+	var cmd *exec.Cmd
 
-    if runtime.GOOS == "windows" {
-        cmd = exec.Command("cmd", "/c", "cls")
-    } else {
-        cmd = exec.Command("clear")
-    }
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
 
-    cmd.Stdout = os.Stdout
-    cmd.Run()
-} */
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
 
 func startRepl(cfg *config) {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -69,11 +77,32 @@ func startRepl(cfg *config) {
 
 		scanner.Scan()
 		input := scanner.Text()
+		words := cleanInput(input)
 
-		//clearTerminal()
-		err := cliCommands[input].callback(cfg)
-		if err != nil {
-			fmt.Println("Error: ", err)
+		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
 		}
+
+		command, exists := cliCommands[commandName]
+		if exists {
+			clearTerminal()
+			err := command.callback(cfg, args...)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+			continue
+		} else {
+			fmt.Println("unknown command")
+			continue
+		}
+
 	}
+}
+
+func cleanInput(input string) []string {
+	output := strings.ToLower(input)
+	words := strings.Fields(output)
+	return words
 }
