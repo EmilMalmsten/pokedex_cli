bc05d049f2a6e0657d00ecf5e0e5b00a02b867ef
Emil Malmsten <emil_malmsten@hotmail.com>
2023-04-24 08:56:51 +0700

    add explore area api request

---
+++ internal/pokeapi/explore_area.go
@@ -0,0 +1,106 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Location struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int           `json:"chance"`
+				ConditionValues []interface{} `json:"condition_values"`
+				MaxLevel        int           `json:"max_level"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				MinLevel int `json:"min_level"`
+			} `json:"encounter_details"`
+			MaxChance int `json:"max_chance"`
+			Version   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
+func (c *Client) ExploreArea(locationName string) (Location, error) {
+	fullURL := baseURL + "/location-area/" + locationName
+
+	data, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit
+		locationResp := Location{}
+		err := json.Unmarshal(data, &locationResp)
+		if err != nil {
+			return locationResp, err
+		}
+		return locationResp, nil
+	}
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return Location{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return Location{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 399 {
+		return Location{}, fmt.Errorf("Response failed with status code: %v", res.StatusCode)
+	}
+
+	data, err = io.ReadAll(res.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	locationResp := Location{}
+	err = json.Unmarshal(data, &locationResp)
+	if err != nil {
+		return locationResp, err
+	}
+
+	c.cache.Add(fullURL, data)
+
+	return locationResp, nil
+}
